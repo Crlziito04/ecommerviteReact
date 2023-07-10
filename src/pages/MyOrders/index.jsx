82161573bd69e6ba6cbbf1faafe4c2715e66b9cc
Carlos Molina <carlos.jmr90@gmail.com>
2023-07-10 19:09:26 -0300

    [FIX URL ORDERS]

--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -1,5 +1,24 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { OrdersCard } from "../../components/OrdersCard";
+import { ShoppingCartContext } from "../../context";
+
 function MyOrders() {
-  return <div className="bg-red-100">MyOrders</div>;
+  const context = useContext(ShoppingCartContext);
+  return (
+    <div>
+      <h1>MyOrders</h1>
+
+      {context.order.map((order, index) => (
+        <Link key={index} to={`/my-orders/${index}`}>
+          <OrdersCard
+            totalPrice={order.totalPrice}
+            totalProducts={order.totalProducts}
+          />
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export { MyOrders };
